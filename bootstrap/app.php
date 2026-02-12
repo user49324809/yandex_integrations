cd802a6840feeaf5a84f7011420861a97c3af697
Наталья <inna6903zaharova@yandex.ru>
2026-02-12 19:22:11 +0300

    restore index.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
 
     ->withExceptions(function (Exceptions $exceptions): void {
