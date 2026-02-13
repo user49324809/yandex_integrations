ebd512fa75d5e0ce2830d578d3601a18ec9e73b3
Наталья <inna6903zaharova@yandex.ru>
2026-02-14 00:22:07 +0300

    force redeploy

--- app/Models/Integration.php
+++ app/Models/Integration.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Integration extends Model
 {
-    protected $fillable = [
+    /*protected $fillable = [
         'user_id',
         'yandex_url',
         'company_id'
-    ];
+    ];*/
+    protected $guarded = [];
 }
